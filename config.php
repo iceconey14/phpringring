071470f1abee13c86587ea0724ae23312f90f5fc
iceconey14 <147879255+iceconey14@users.noreply.github.com>
2025-05-12 20:39:20 -0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// dont worry, you dont have to fill this out. the install.php file does it for you
+define('DB_TYPE', 'mysql'); // or 'sqlite', 'pgsql', 'mongodb'
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'webring');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('SQLITE_PATH', __DIR__ . '/webring.sqlite');
+?>
